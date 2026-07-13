ffce78f02696aefee4eef7f771b9d9012414d1b8
sl2014we-rgb <sl2014we@gmail.com>
2026-07-13 23:08:55 +0300

    Update deepseek.js

--- api/deepseek.js
+++ api/deepseek.js
@@ -28,7 +28,7 @@ module.exports = function handler(req, res) {
 
         // Прямой системный кабель на официальный эндпоинт Китая
        const options = {
-            hostname: '://deepseek.com', // СТРОГО ТАК! Никаких "://", никаких "https", только чистый домен
+            hostname: 'deepseek.com', 
             port: 443,
             path: '/v1/chat/completions',
             method: 'POST',
